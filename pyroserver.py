5dc036cca734dccf7cf020404b546d98621cd544
Chen Zhao <zhao58@sheridancollege.ca>
2026-03-24 14:30:27 -0400

    init commit + pyro4 server

---
+++ pyroserver.py
@@ -0,0 +1,50 @@
+import Pyro4
+
+class Server:
+    # @Pyro4.expose, means that the preceding method will be remotely accessible.
+    @Pyro4.expose
+    def add(self, op1, op2):
+        return op1 + op2
+
+    @Pyro4.expose
+    def sub(self, op1, op2):
+        return op1 + op2
+
+    @Pyro4.expose
+    def mult(self, op1, op2):
+        return op1 + op2
+
+    @Pyro4.expose
+    def div(self, op1, op2):
+        return op1 + op2
+
+    @Pyro4.expose
+    def mod(self, op1, op2):
+        return op1 % op2
+
+    @Pyro4.expose
+    def sqrt(self, op1):
+        return op1 ** 0.5
+
+def startServer():
+    # Next, build the server instance of the Server class:
+    server = Server()
+
+    # Then, define the Pyro4 daemon:
+    daemon = Pyro4.Daemon()
+
+    # To execute this script, we must run a Pyro4 statement to locate a nameserver:
+    ns = Pyro4.locateNS()
+
+    # Register the object server as Pyro object; it will only be known inside the Pyro daemon:
+    uri = daemon.register(server)
+    # Now, we can register the object server with a name in the nameserver:
+    ns.register("server", uri)
+
+    # The function ends with a call to the daemon's request Loop method. This starts the event loop of the server and
+    # waits for calls:
+    print("Ready. Object uri =", uri)
+    daemon.requestLoop()
+
+if __name__ == "__main__":
+    startServer()
